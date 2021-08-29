7a1c7d404909b32904c329eedab83dbbccdfcb56
Random-Access-Exomemory <88039945+Random-Access-Exomemory@users.noreply.github.com>
2021-08-29 13:01:34 +0100

    Added the command for signing the apk

--- build.sh
+++ build.sh
@@ -1,17 +1,25 @@
+#!/bin/sh
+
 echo "Decompiling APK"
 echo
-apktool d discord-beta.apk
+apktool d -r Aliucord.apk
 echo
-cd discord-beta/
+cd Aliucord/
 echo "Running the patcher"
 echo
 python3 ../patcher.py
 echo
 cd ..
 echo "Recompiling the APK with patches"
-apktool b -f -d discord-beta/ -o fosscord.unsigned.apk
+apktool b -f -d Aliucord/ -o fosscord.unsigned.apk
 echo
 echo "APK compiled"
 # Command to sign the apk
-echo
-echo "I am not able to sign the APK so you'll need to do it manually"
+if [ ! -f "keystore.jks" ]; then
+ echo "Keystore file doesn't exist, generating"
+ keytool -genkey -v -keystore keystore.jks -keyalg RSA -keysize 2048 -validity 10000
+else
+ echo "Keystore file exists, skipping generation"
+fi
+echo "Signing apk"
+apksigner sign --ks keystore.jks --out fosscord.signed.apk fosscord.unsigned.apk
