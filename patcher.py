5b16fa737db5be79934e214870a7be1531ec2a58
Random-Access-Exomemory <88039945+Random-Access-Exomemory@users.noreply.github.com>
2021-08-29 13:20:20 +0100

    Fix patcher.py and make it customisable

--- patcher.py
+++ patcher.py
@@ -1,15 +1,26 @@
 from os import walk, path
+from json import load as jload
+
+with open('settings.json') as f:
+    config = jload(f)
 
 # Things that need some renaming to actually work correctly should be edited and reviewed here every update
 bugfixes = [
 ]
 
 # Basic replacements throughout the code to replace discord routes with fosscord routes
+# NOTE: Order of replacements is VERY important and will probably have to do stupid stuff to make it customisable
 replacements = [
-  ('discord.com', 'dev.fosscord.com'), # discord.com to dev.fosscord.com
-  ('discord.gg', 'dev.fosscord.com/invite'), # discord.gg to dev.fosscord.com/invite
-  ('DEBUG:Z = false', 'DEBUG:Z = true') # Enabling debugging
+  ('cdn.discord.com', 'REPLACE CDN URL HERE'),
+  ('gateway.discord.gg', 'REPLACE GATEWAY URL HERE')
+  ('discord.com', config['base_url']), # discord.com to the base url of settungs.json
+  ('discord.gg', config['invite_url']), # discord.gg to the invite url
+  ('REPLACE CDN URL HERE', config['cdn_url']),
+  ('REPLACE GATEWAY URL HERE', config['gateway_url']),
 ]
+
+if config.get('debug').lower():
+    replacements.append(("DEBUG:Z = false", "DEBUG:Z = true")) # Enables debug if it's true in the config
 
 # TODO: Potentially add support for patching files with .patch files?
 
