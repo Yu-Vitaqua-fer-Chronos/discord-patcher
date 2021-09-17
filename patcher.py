b09eead64c10434e9f50382af0e464b7ac309223
Random Access Exomemory <Random.Access.Exomemory@gmail.com>
2021-09-17 17:17:16 +0100

    Changes here

--- patcher.py
+++ patcher.py
@@ -27,11 +27,11 @@ bugfixes = [
 # Basic replacements throughout the code to replace discord routes with fosscord routes
 # NOTE: Order of replacements is VERY important and will probably have to do stupid stuff to make it customisable
 replacements = [
-  ('cdn.discordapp.com', config['cdn_url']),
-  ('gateway.discord.gg', config['gateway_url']),
-  ('discord.com', config['base_url']), # discord.com to the base url of settungs.json
-  ('discordapp.com', config['base_url']), # Extra change just in case discordapp is still used in the code somewhere
-  ('discord.gg', config['invite_url']), # discord.gg to the invite url
+  ('https://cdn.discordapp.com', 'https://'+config['cdn_url']),
+  ('https://gateway.discord.gg', 'https://'+config['gateway_url']),
+  ('https://discord.com', 'https://'+config['base_url']), # discord.com to the base url of settungs.json
+  ('https://discordapp.com', 'https://'+config['base_url']), # Extra change just in case discordapp is still used in the code somewhere
+  ('https://discord.gg', 'https://'+config['invite_url']), # discord.gg to the invite url
 ]
 
 if config.get('debug'):
@@ -39,35 +39,44 @@ if config.get('debug'):
 
 # TODO: Potentially add support for patching files with .patch files?
 
-for root, _, files in walk('.'):
-    for file in files:
-        fpath = path.join(root, file)
-        try:
-            with open(fpath) as f:
-                data = tmp = f.read()
-            for bugfix in bugfixes:
-                data = data.replace(*bugfix)
-            for replacement in replacements:
-                data = data.replace(*replacement)
-            if tmp != data:
-                with open(fpath, 'w+') as f:
-                    f.write(data)
-                print(f"Applied patches to `{fpath}`")
-        except UnicodeDecodeError:
-            pass
+system("mv AndroidManifest.xml ..")
 
-if config['package_name']:
-    with open('AndroidManifest.xml') as f:
-        manifest = f.read()
-    manifest = manifest.replace(config['original_package_name'], config['package_name'])
-    print("Applied patches to `AndroidManifest.xml`")
+def patch(folder):
+    for root, _, files in walk(path.join('.', folder)):
+        for file in files:
+            fpath = path.join(root, file)
+            try:
+                with open(fpath) as f:
+                    data = tmp = f.read()
+                for bugfix in bugfixes:
+                    data = data.replace(*bugfix)
+                for replacement in replacements:
+                    data = data.replace(*replacement)
+                if tmp != data:
+                    with open(fpath, 'w+') as f:
+                        f.write(data)
+                    print(f"Applied patches to `{fpath}`")
+            except UnicodeDecodeError:
+                pass
+
+patch('smali')
+patch('smali_classes2')
+patch('smali_classes3')
+
+system("mv ../AndroidManifest.xml .")
+
+#if config['package_name']:
+#   with open('AndroidManifest.xml') as f:
+#        manifest = f.read()
+#    manifest = manifest.replace(config['original_package_name'], config['package_name'])
+#    print("Applied patches to `AndroidManifest.xml`")
 print("\nFinished applying patches\n\nRecompiling APK...")
 chdir('..')
-system('apktool b -f -d discord/ -o fosscord.unsigned.apk')
+system('apktool b discord/ -o fosscord.unsigned.apk')
 print("\nRecompiled APK.\nChecking if keystore exists...")
 if not path.exists('keystore.jks'):
     print("\nNo keystore exists, creating one...")
     system('keytool -genkey -v -keystore keystore.jks -keyalg RSA -keysize 2048 -validity 10000')
 print("\nSigning APK...")
 system('apksigner sign --ks keystore.jks --out fosscord.signed.apk fosscord.unsigned.apk')
-print("\nFinished signing the APK, the APK's name is `fosscord.signed.apk`. You should be able to install this alongside your current discord installation.")
+print("\nFinished signing the APK, the APK's name is `fosscord.signed.apk`.")
